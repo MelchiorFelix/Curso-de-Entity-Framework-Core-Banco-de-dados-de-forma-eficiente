29fc2fadba627a6705c31808204defc11b90183a
MelchiorFelix <melchior.alencar@gmail.com>
2020-08-03 09:52:05 -0300

    Primeiro uso da ferramenta

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,20 +10,37 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
+            //GravarUsandoAdoNet();
+            GravarUsandoEntity();
         }
 
-        private static void GravarUsandoAdoNet()
+        private static void GravarUsandoEntity()
         {
             Produto p = new Produto();
             p.Nome = "Harry Potter e a Ordem da Fênix";
             p.Categoria = "Livros";
             p.Preco = 19.89;
 
-            using (var repo = new ProdutoDAO())
+            using (var contexto = new LojaContext())
             {
-                repo.Adicionar(p);
+                contexto.Produtos.Add(p);
+                contexto.SaveChanges();
             }
         }
     }
+
+        //private static void GravarUsandoAdoNet()
+        //{
+        //    Produto p = new Produto();
+        //    p.Nome = "Harry Potter e a Ordem da Fênix";
+        //    p.Categoria = "Livros";
+        //    p.Preco = 19.89;
+
+        //    using (var repo = new ProdutoDAO())
+        //    {
+        //        repo.Adicionar(p);
+        //    }
+        //}
+    
 }
+
