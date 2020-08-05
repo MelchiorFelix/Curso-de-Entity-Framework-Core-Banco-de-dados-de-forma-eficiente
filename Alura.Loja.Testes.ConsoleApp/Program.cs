ed8461eaaf0a38cbe98bf06876184aa84ed27bef
MelchiorFelix <melchior.alencar@gmail.com>
2020-08-05 10:03:49 -0300

    Organizando o código de acesso em um DAO

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -24,12 +24,11 @@ namespace Alura.Loja.Testes.ConsoleApp
             GravarUsandoEntity();
             RecuperaProdutos();
             // atualiazar o produto
-            using (var repo = new LojaContext())
+            using (var repo = new ProdutoDAOEntity())
             {
-                Produto primeiro = repo.Produtos.First();
+                Produto primeiro = repo.Produtos().First();
                 primeiro.Nome = "Harry Poter - Editado";
-                repo.Produtos.Update(primeiro);
-                repo.SaveChanges();
+                repo.Atualizar(primeiro);
 
             }
             RecuperaProdutos();
@@ -37,23 +36,22 @@ namespace Alura.Loja.Testes.ConsoleApp
 
         private static void ExcluirProdutos()
         {
-            using (var repo = new LojaContext())
+            using (var repo = new ProdutoDAOEntity())
             {
-                IList<Produto> produtos = repo.Produtos.ToList();
+                IList<Produto> produtos = repo.Produtos();
                 foreach (var item in produtos)
                 {
-                    repo.Produtos.Remove(item);
+                    repo.Remover(item);
                 }
 
-                repo.SaveChanges();
             }
         }
 
         private static void RecuperaProdutos()
         {
-            using (var contexto = new LojaContext())
+            using (var contexto = new ProdutoDAOEntity())
             {
-                IList<Produto> produtos = contexto.Produtos.ToList();
+                IList<Produto> produtos = contexto.Produtos();
                 Console.WriteLine("Foram encontrados {0} produto(s)", produtos.Count);
                 foreach (var item in produtos)
                 {
@@ -72,10 +70,9 @@ namespace Alura.Loja.Testes.ConsoleApp
             p.Categoria = "Livros";
             p.Preco = 19.89;
 
-            using (var contexto = new LojaContext())
+            using (var contexto = new ProdutoDAOEntity())
             {
-                contexto.Produtos.Add(p);
-                contexto.SaveChanges();
+                contexto.Adicionar(p);
             }
         }
     }
